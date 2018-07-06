8eb961c8764c77d0bef089569b672c563fbef8de
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-07-05 21:44:30 -0400

    Drop `libuuid` on macOS
    macOS does not need `libuuid` as it provides UUID as part of the system.
    Turns out `fontconfig` has a bug where it fails to recognize this and
    errors out on macOS. This bug is limited to `fontconfig` version
    `2.13.0`. Hence the included workaround should be dropped after that.
    
    ref: https://bugs.freedesktop.org/show_bug.cgi?id=105366

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,16 @@
 if [[ $(uname) == 'Darwin' ]];
 then
     export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+
+    #############################################################
+    # Workaround for `libuuid` bug in 2.13.0.                   #
+    # Drop on subsequent `fontconfig` release.                  #
+    #                                                           #
+    # ref: https://bugs.freedesktop.org/show_bug.cgi?id=105366  #
+    #############################################################
+    export UUID_CFLAGS=" "
+    export UUID_LIBS=" "
+    export PKGCONFIG_REQUIRES_PRIVATELY="${PKGCONFIG_REQUIRES_PRIVATELY} uuid"
 elif [[ $(uname) == 'Linux' ]];
 then
     export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
